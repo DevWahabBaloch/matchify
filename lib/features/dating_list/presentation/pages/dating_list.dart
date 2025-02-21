fca1d999b67f2f86ea87cdf2ef97df97538b92c1
DevWahabBaloch <abdulwahabshafi001@gmail.com>
2025-02-22 01:38:07 +0500

    Padding Issue Resolved

--- lib/features/dating_list/presentation/pages/dating_list.dart
+++ lib/features/dating_list/presentation/pages/dating_list.dart
@@ -27,80 +27,89 @@ class _DatingListState extends State<DatingList> {
     double width = MediaQuery.sizeOf(context).width;
 
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: width * 0.02),
-        child: SingleChildScrollView(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.02),
           child: Column(
             children: [
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: height * 0.05),
-                child: Container(
-                  height: height * 0.17,
-                  decoration: BoxDecoration(color: AppColors.primaryColor, borderRadius: BorderRadius.circular(20)),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: width * 0.02),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: width * 0.06, vertical: height * 0.025),
-                          child: Row(
-                            children: [
-                              Image.asset(
-                                height: height * 0.02,
-                                Assets.images.arrowLeft.path,
-                                color: AppColors.secondaryColor,
-                              ),
-                              SizedBox(width: width * 0.24),
-                              const Text(
-                                AppStrings.datingList,
-                                style: TextStyle(color: AppColors.secondaryColor, fontSize: 20, fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Card(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-                          elevation: 2.0,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              hintText: 'Search',
-                              prefixIcon: Image.asset(Assets.images.search.path, height: height * 0.03),
-                              border: InputBorder.none,
+              Container(
+                height: height * 0.17,
+                decoration: BoxDecoration(
+                  color: AppColors.primaryColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * 0.02),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: width * 0.06, vertical: height * 0.02),
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              height: height * 0.02,
+                              Assets.images.arrowLeft.path,
+                              color: AppColors.secondaryColor,
                             ),
+                            SizedBox(width: width * 0.24),
+                            const Text(
+                              AppStrings.datingList,
+                              style: TextStyle(
+                                color: AppColors.secondaryColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Card(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                        elevation: 2.0,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Search',
+                            prefixIcon: Image.asset(
+                              Assets.images.search.path,
+                              height: height * 0.03,
+                            ),
+                            border: InputBorder.none,
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              BlocBuilder<DatingCubit, DatingState>(
-                builder: (context, state) {
-                  if (state is LoadingState) {
-                    return const CircularProgressIndicator();
-                  } else if (state is LoadedState) {
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: state.events.length,
-                      itemBuilder: (context, index) {
-                        final event = state.events[index];
-                        return EventTile(
-                          name: event.fullName,
-                          title: event.title,
-                          distance: event.distance,
-                          location: event.country,
-                          startDate: event.birthDate,
-                          time: event.time,
-                          image: event.profilePicture,
-                        );
-                      },
-                    );
-                  } else if (state is FailureState) {
-                    return Center(child: Text(state.message));
-                  }
-                  return Container();
-                },
+              SizedBox(height: height * 0.02),
+              Expanded(
+                child: BlocBuilder<DatingCubit, DatingState>(
+                  builder: (context, state) {
+                    if (state is LoadingState) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (state is LoadedState) {
+                      return ListView.builder(
+                        physics: const BouncingScrollPhysics(),
+                        itemCount: state.events.length,
+                        itemBuilder: (context, index) {
+                          final event = state.events[index];
+                          return EventTile(
+                            name: event.fullName,
+                            title: event.title,
+                            distance: event.distance,
+                            location: event.country,
+                            startDate: event.birthDate,
+                            time: event.time,
+                            image: event.profilePicture,
+                          );
+                        },
+                      );
+                    } else if (state is FailureState) {
+                      return Center(child: Text(state.message));
+                    }
+                    return Container();
+                  },
+                ),
               ),
             ],
           ),
